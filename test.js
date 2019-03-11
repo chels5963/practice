0db4382a6bfea0d39e31ab2625015e81be68b99b
chels5963 <chels5963@gmail.com>
2019-03-11 17:25:45 +0900

    First commit.

---
+++ test.js
@@ -0,0 +1,24 @@
+function onButtonClick() {
+    
+    let li = document.createElement('li');
+    let inputText = document.forms.form1.taskInfo.value;
+    li.innerText = inputText;
+    li.className = 'list-group-item';
+
+    document.getElementById('taskArea').appendChild(li);
+    
+};
+
+
+
+
+
+
+
+    
+    
+ 
+
+
+
+
